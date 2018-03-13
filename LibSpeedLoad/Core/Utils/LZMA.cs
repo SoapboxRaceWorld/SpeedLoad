c5c33f59df78727241b4aab1db5e571ae19f0d2c
metonator <metonator@gmail.com>
2018-03-13 02:58:01 +0100

    -AutoUncompress +LZMAUncompress

--- LibSpeedLoad/Core/Utils/LZMA.cs
+++ LibSpeedLoad/Core/Utils/LZMA.cs
@@ -5,29 +5,8 @@ namespace LibSpeedLoad.Core.Utils
 {
     public static class LZMA
     {
-        private static class Win32
-        {
-            [DllImport("EasyLZMA.dll", CharSet = CharSet.None, ExactSpelling = false)]
-            public static extern int LzmaUncompress(byte[] dest, ref IntPtr destLen, byte[] src, ref IntPtr srcLen,
-                byte[] outProps, IntPtr outPropsSize);
-        }
-
-        /**
-         * EasyLZMA is used for unix systems, for Win32 we can fall back to the original LZMA.dll for now.
-         */
-        private static class Unix
-        {
-            [DllImport("EasyLZMA", CharSet = CharSet.None, ExactSpelling = false)]
-            public static extern int LzmaUncompress(byte[] dest, ref IntPtr destLen, byte[] src, ref IntPtr srcLen,
-                byte[] outProps, IntPtr outPropsSize);
-        }
-
-        public static int AutoUncompress(ref byte[] dest, ref IntPtr destLen, byte[] src, ref IntPtr srcLen,
-            byte[] outProps, IntPtr outPropsSize)
-        {
-            return DebugUtil.IsLinux ? 
-                Unix.LzmaUncompress(dest, ref destLen, src, ref srcLen, outProps, outPropsSize) : 
-                Win32.LzmaUncompress(dest, ref destLen, src, ref srcLen, outProps, outPropsSize);
-        }
+        [DllImport("EasyLZMA", CharSet = CharSet.None, ExactSpelling = false)]
+        public static extern int LzmaUncompress(byte[] dest, ref IntPtr destLen, byte[] src, ref IntPtr srcLen,
+            byte[] outProps, IntPtr outPropsSize);
     }
 }
