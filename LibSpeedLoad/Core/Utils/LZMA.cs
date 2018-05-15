576e84c2900ee63e5fa822338a69567cc9fddcb9
heyitsleo <coderleo42+leocodes@gmail.com>
2018-05-15 15:48:01 -0400

    Add explicit calling convention to LZMA

--- LibSpeedLoad/Core/Utils/LZMA.cs
+++ LibSpeedLoad/Core/Utils/LZMA.cs
@@ -5,7 +5,7 @@ namespace LibSpeedLoad.Core.Utils
 {
     public static class LZMA
     {
-        [DllImport("EasyLZMA", CharSet = CharSet.None, ExactSpelling = false)]
+        [DllImport("EasyLZMA", CharSet = CharSet.None, ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
         public static extern int LzmaUncompress(byte[] dest, ref IntPtr destLen, byte[] src, ref IntPtr srcLen,
             byte[] outProps, IntPtr outPropsSize);
     }
